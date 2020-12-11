4c2e9aee333569f0c5778d6dbc10f981070b8ef1
ginakui <developerkui@outlook.com>
2020-12-11 07:29:25 -0500

    fix techSlice pending thunk action creators. refactor TechListModal using useDispatch and useSelector hooks

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getTechs } from './techSlice';
 import TechItem from './TechItem';
 
 
-const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
-  useEffect(() => {
-    getTechs();
-    // eslint-disable-next-line
-  }, []);
+const TechListModal = () => {
+  const dispatch = useDispatch();
+  dispatch(getTechs);
+  const techs = useSelector(state => state.tech.techs);
+  const status = useSelector(state => state.tech.status);
 
   return (
     <div id='tech-list-modal' className='modal modal-fixed-footer'>
       <div className='modal-content'>
         <h4>Technician List</h4>
         <ul className='collection'>
-          {!loading &&
+          { status === 'succeeded' &&
             techs !== null &&
             techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
         </ul>
@@ -25,16 +24,4 @@ const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
   );
 };
 
-TechListModal.propTypes = {
-  tech: PropTypes.object.isRequired,
-  getTechs: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-  tech: state.tech
-});
-
-export default connect(
-  mapStateToProps,
-  { getTechs }
-)(TechListModal);
+export default TechListModal;
--- src/components/techs/techSlice.js
+++ src/components/techs/techSlice.js
@@ -2,15 +2,14 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   techs: [],
-  loading: false,
+  status: 'idle', //'loading','succeeded'
   error: null
 };
 //@TODO: - loading field should use enums, loading/idle/error...etc
 
 const getTechs = createAsyncThunk(
   'tech/getTechs',
-  async (thunkAPI) => {
-    setLoading(); //may cause problem
+  async () => {
     const res = await fetch('/techs');
     const data = await res.json();
     return data;
@@ -20,7 +19,6 @@ const getTechs = createAsyncThunk(
 const addTech = createAsyncThunk(
   'tech/addTech',
   async (tech, thunkAPI) => {
-    setLoading();
     const res = await fetch('/techs', {
       method: 'POST',
       body: JSON.stringify(tech),
@@ -36,7 +34,6 @@ const addTech = createAsyncThunk(
 const deleteTech = createAsyncThunk(
   'tech/deleteTech',
   async (id, thunkAPI) => {
-    setLoading();
     await fetch(`/techs/${id}`, {
       method: 'DELETE'
     });
@@ -51,25 +48,30 @@ const techSlice = createSlice({
     setLoading(state, action) {
       // ✅ This "mutating" code is okay
       // createSlice use Immer to update to keep the immutability
-      return {
-        ...state,
-        loading: true
-      }
+      state.status = 'loading'
     }
   },
   extraReducers: {
-    //@TODO: - loading should be set here as .pending
+    [getTechs.pending]: (state, action) => {
+      state.status = 'loading';
+    },
     [getTechs.fulfilled]: (state, action) => {
       state.techs = action.payload;
-      state.loading = false;
+      state.status = 'succeeded';
+    },
+    [addTech.pending]: (state, action) => {
+      state.status = 'loading';
     },
     [addTech.fulfilled]: (state, action) => {
       state.techs.push(action.payload);
-      state.loading = false;
+      state.status = 'succeeded';
+    },
+    [deleteTech.pending]: (state, action) => {
+      state.status = 'loading';
     },
     [deleteTech.fulfilled]: (state, action) => {
       state.techs = state.techs.filter(tech => tech.id !== action.payload);
-      state.loading = false;
+      state.status = 'succeeded';
     }
   }
 });
